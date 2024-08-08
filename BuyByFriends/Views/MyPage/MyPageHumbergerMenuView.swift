e4b47e95d189741be100097623ebdce8abbc217d
kotathefriends <5070kouta@gmail.com>
2024-08-08 17:32:30 +0900

    MyPageHumbergerMenuView のデザインのみ変更

--- BuyByFriends/Views/MyPage/MyPageHumbergerMenuView.swift
+++ BuyByFriends/Views/MyPage/MyPageHumbergerMenuView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MyPageHumbergerMenuView: View {
     @EnvironmentObject var path: Path
+    //    @Environment(\.openURL) private var openURL
 
     init() {
         if #available(iOS 14.0, *) {
@@ -24,54 +25,195 @@ struct MyPageHumbergerMenuView: View {
 
     var body: some View {
         NavigationStack(path: $path.path) {
-            List {
-                ForEach(MyPageHumbergerMenus.allCases, id: \.self) { menu in
-                    NavigationLink(value: menu) {
-                        HStack {
-                            switch menu {
-                            case .withdrawal:
-                                Image(systemName: "yensign.circle")
-                                Text("売上金の出金")
-                            case .purchasedItems:
-                                Asset.Icons.shoppingBag.swiftUIImage
-                                Text("購入した商品")
-                            case .likedItems:
-                                Asset.Icons.heart.swiftUIImage
-                                Text("いいねした商品")
-                            case .bookmarkedItems:
-                                Asset.Icons.bookmark.swiftUIImage
-                                Text("保存した商品")
-                            case .shippingAdress:
-                                Asset.Icons.truck.swiftUIImage
-                                Text("お届け先情報")
-                            case .termsOfService:
-                                Asset.Icons.note.swiftUIImage
-                                Text("利用規約")
-                            case .privacyPolicy:
-                                Asset.Icons.note.swiftUIImage
-                                Text("プライバシーポリシー")
-                            case .notation:
-                                Asset.Icons.note.swiftUIImage
-                                Text("特定商取引に基づく表記")
-                            case .signout:
-                                Asset.Icons.signout.swiftUIImage
-                                Text("ログアウト")
-                            case .deleteAccount:
-                                Asset.Icons.circleCross.swiftUIImage
-                                Text("アカウントの削除")
-                            case .contact:
-                                Asset.Icons.envelope.swiftUIImage
-                                Text("お問い合わせ")
+            VStack {
+                List {
+                    Section(header: Text("商品管理").foregroundColor(Asset.Colors.secondText.swiftUIColor)) {
+                        ForEach(MyPageHumbergerMenus.allCases.prefix(5), id: \.self) { menu in
+                            NavigationLink(value: menu) {
+                                HStack {
+                                    switch menu {
+                                    case .withdrawal:
+                                        Asset.Icons.yen.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("売上金の出金")
+                                    case .purchasedItems:
+                                        Asset.Icons.shoppingBag.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("購入した商品")
+                                    case .likedItems:
+                                        Asset.Icons.heart.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("いいねした商品")
+                                    case .bookmarkedItems:
+                                        Asset.Icons.bookmark.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("保存した商品")
+                                    case .shippingAdress:
+                                        Asset.Icons.truck.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("お届け先情報")
+                                    default:
+                                        EmptyView()
+                                    }
+                                }
+                                .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                                .bold()
                             }
                         }
-                        .bold()
+                        .listRowSeparator(.hidden)
+                    }
+                    Section(header: Text("基本情報").foregroundColor(Asset.Colors.secondText.swiftUIColor)) {
+                        ForEach(MyPageHumbergerMenus.allCases.dropFirst(5).prefix(4), id: \.self) { menu in
+                            NavigationLink(value: menu) {
+                                HStack {
+                                    switch menu {
+
+                                    case .rateApp:
+                                        Asset.Icons.star.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("BuyByFriends を応援する")
+                                    case .termsOfService:
+                                        Asset.Icons.note.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("利用規約")
+                                    case .privacyPolicy:
+                                        Asset.Icons.note.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("プライバシーポリシー")
+                                    case .notation:
+                                        Asset.Icons.note.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("特定商取引に基づく表記")
+                                    default:
+                                        EmptyView()
+                                    }
+                                }
+                                .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                                .bold()
+                            }
+                        }
+                        .listRowSeparator(.hidden)
+                    }
+                    Section(header: Text("ヘルプ").foregroundColor(Asset.Colors.secondText.swiftUIColor)) {
+                        ForEach(MyPageHumbergerMenus.allCases.dropFirst(9).prefix(3), id: \.self) { menu in
+                            NavigationLink(value: menu) {
+                                HStack {
+                                    switch menu {
+
+                                    case .contact:
+                                        Asset.Icons.mail.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("お問い合わせ")
+                                    case .reportBug:
+                                        Asset.Icons.bug.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("バグを報告")
+                                    case .faq:
+                                        Asset.Icons.question.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("よくある質問")
+
+                                    default:
+                                        EmptyView()
+                                    }
+                                }
+                                .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                                .bold()
+                            }
+                        }
+                        .listRowSeparator(.hidden)
+                    }
+                    Section(header: Text("SNS").foregroundColor(Asset.Colors.secondText.swiftUIColor)) {
+                        ForEach(MyPageHumbergerMenus.allCases.dropFirst(12).prefix(3), id: \.self) { menu in
+                            NavigationLink(value: menu) {
+                                HStack {
+                                    switch menu {
+                                    case .tiktok:
+                                        Asset.Icons.tiktok.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("TikTok")
+                                    case .instagram:
+                                        Asset.Icons.instagram.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("Instagram")
+                                    case .twitter:
+                                        Asset.Icons.twitter.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("X")
+
+                                    default:
+                                        EmptyView()
+                                    }
+                                }
+                                .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                                .bold()
+                            }
+                        }
+                        .listRowSeparator(.hidden)
+                    }
+                    Section(header: Text("その他").foregroundColor(Asset.Colors.secondText.swiftUIColor)) {
+                        ForEach(MyPageHumbergerMenus.allCases.dropFirst(15), id: \.self) { menu in
+                            NavigationLink(value: menu) {
+                                HStack {
+                                    switch menu {
+                                    case .signout:
+                                        Asset.Icons.signoutAlert.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("ログアウト")
+                                    case .deleteAccount:
+                                        Asset.Icons.circleCrossAlert.swiftUIImage
+                                            .resizable()
+                                            .frame(width: 32, height: 32)
+                                        Text("アカウントの削除")
+                                    default:
+                                        EmptyView()
+                                    }
+                                }
+                                .foregroundColor(Asset.Colors.alert.swiftUIColor)
+                                .bold()
+                            }
+                        }
+                        .listRowSeparator(.hidden)
+                    }
+
+                    Section {
+                        VStack(spacing: 8) {
+                            Asset.Icons.logo.swiftUIImage
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 20)
+                            Text("Version \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown")")
+                                .font(.subheadline)
+                                .foregroundColor(Asset.Colors.thirdText.swiftUIColor)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.top, 8)
+                        .padding(.bottom, -20)
+                        .listRowBackground(Color.clear)
                     }
                 }
-                .listRowSeparator(.hidden)
             }
-            .listStyle(.plain)
+            .listStyle(.insetGrouped)
+            .scrollContentBackground(.hidden)
+            .background(Asset.Colors.lightGray.swiftUIColor)
             .navigationBarTitleDisplayMode(.inline)
-            .navigationTitle("メニュー")
+            .navigationTitle("設定")
             .navigationDestination(for: MyPageHumbergerMenus.self) { menu in
                 switch menu {
                 case .withdrawal:
@@ -84,18 +226,31 @@ struct MyPageHumbergerMenuView: View {
                     BookmarkedItemsView()
                 case .shippingAdress:
                     ShippingAdressView()
+                case .rateApp:
+                    EmptyView()
                 case .termsOfService:
                     EmptyView()
                 case .privacyPolicy:
                     EmptyView()
                 case .notation:
                     EmptyView()
+                case .contact:
+                    ContactView()
+                case .reportBug:
+                    EmptyView()
+                case .faq:
+                    EmptyView()
+                case .tiktok:
+                    EmptyView()
+                case .instagram:
+                    EmptyView()
+                case .twitter:
+                    EmptyView()
                 case .signout:
                     EmptyView()
                 case .deleteAccount:
                     DeleteAccountView()
-                case .contact:
-                    ContactView()
+
                 }
             }
             .navigationDestination(for: Withdraw.self) { withdraw in
@@ -104,23 +259,29 @@ struct MyPageHumbergerMenuView: View {
         }
     }
 }
-
 enum MyPageHumbergerMenus: CaseIterable {
     case withdrawal
     case purchasedItems
     case likedItems
     case bookmarkedItems
     case shippingAdress
+    case rateApp
     case termsOfService
     case privacyPolicy
     case notation
+    case contact
+    case reportBug
+    case faq
+    case tiktok
+    case instagram
+    case twitter
     case signout
     case deleteAccount
-    case contact
 }
 
 struct MyPageHumbergerMenuView_Previews: PreviewProvider {
     static var previews: some View {
         MyPageHumbergerMenuView()
+            .environmentObject(Path())
     }
 }
