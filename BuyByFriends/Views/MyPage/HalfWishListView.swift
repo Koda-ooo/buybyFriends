d377f1cd6e82025bb28c4bfe748d0c58ab1b8c66
myammm <dmy.choco@me.com>
2024-08-10 21:49:23 +0900

    ウィッシュリストハーフモーダルの未登録時のUI作成

--- BuyByFriends/Views/MyPage/HalfWishListView.swift
+++ BuyByFriends/Views/MyPage/HalfWishListView.swift
@@ -18,8 +18,35 @@ struct HalfWishListView: View {
     }
 
     var body: some View {
-        if let key = vm.binding.userInfo.wishList.keys.first {
-            Text("\(key)")
+//        if let key = vm.binding.userInfo.wishList.keys.first {
+//            Text("\(key)")
+//        }
+        VStack(spacing: 24) {
+            if vm.binding.userInfo.wishList.isEmpty {
+                Text("ウィッシュリストを登録しよう")
+                    .font(.system(size: 20, weight: .bold))
+                Text("あなたが気になっている物や\n欲しい物を登録しましょう！\n友達が知らせてくれるかも👀")
+                    .font(.system(size: 16, weight: .semibold))
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(Asset.Colors.silver.swiftUIColor)
+                Button(action: {
+                    // TODO: 登録画面出す
+                }) {
+                    Text("ウィッシュリストを登録する")
+                        .font(.system(size: 14, weight: .bold))
+                        .frame(maxWidth: .infinity, minHeight: 44)
+                        .lineSpacing(3)
+                }
+                .frame(height: 44)
+                .foregroundColor(Asset.Colors.white.swiftUIColor)
+                .background(Asset.Colors.jetBlack.swiftUIColor)
+                .cornerRadius(22)
+                .padding(.horizontal, 20)
+                .padding(.top, 12)
+            } else {
+                // TODO: 登録されてる場合のUI作る
+            }
         }
     }
 }
