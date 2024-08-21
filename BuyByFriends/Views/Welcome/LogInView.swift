7edac0650f06a3aeb0a135c2e520a144cb744da4
kotathefriends <5070kouta@gmail.com>
2024-08-21 21:51:10 +0900

    commit

--- BuyByFriends/Views/Welcome/LogInView.swift
+++ BuyByFriends/Views/Welcome/LogInView.swift
@@ -13,67 +13,70 @@ struct LogInView: View {
     @StateObject private var vm = LogInViewModel()
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("電話番号を入力してください。")
-                .font(.system(size: 20, weight: .black))
-                .padding(.top, 30)
-
-            Text("電話番号は会員登録のみに利用され、\n外部に公表されることは一切ございません。")
-                .font(.system(size: 15, weight: .medium))
-                .padding(.bottom, 30)
-
-            VStack(spacing: 50) {
-                HStack(spacing: 15) {
-                    Text("+81")
-                        .font(.system(size: 20, weight: .light))
-                        .padding(.leading, 20)
-                        .foregroundColor(.black)
-                    Text("|")
-                        .font(.system(size: 50, weight: .thin))
-                        .padding(.bottom, 5)
-                        .foregroundColor(.gray)
-                    TextField("000 0000 0000", text: vm.$binding.phoneNumber)
-                        .font(.system(size: 27, weight: .medium))
-                        .keyboardType(.phonePad)
-                        .foregroundColor(.black)
-                }
-                .background(.white)
-                .cornerRadius(5)
-
-                Button(action: {
-                    UIApplication.shared.closeKeyboard()
-                    vm.input.startToLogIn.send()
-                }) {
-                    Text("認証コードを送信")
-                        .frame(maxWidth: .infinity, minHeight: 70)
-                        .font(.system(size: 20, weight: .medium))
-                }
-                .accentColor(Color.white)
-                .background(!vm.output.isEnabledLogInButton ? Color.gray:Color.black)
-                .cornerRadius(.infinity)
-                .disabled(!vm.output.isEnabledLogInButton)
-            }
-            Spacer()
-        }
-        .frame(maxWidth: UIScreen.main.bounds.width*0.85)
-        .background(
-            Asset.main.swiftUIImage
+        ZStack {
+            Asset.Colors.white.swiftUIColor
                 .edgesIgnoringSafeArea(.all)
-        )
-        .onChange(of: vm.output.isMovedVerificationCodeView) { _ in
-            path.path.append(Destination.Welcome.finishLogIn)
-        }
-        .onTapGesture {
-            UIApplication.shared.closeKeyboard()
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    path.path.removeLast()
-                }) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.black)
+            VStack(alignment: .leading, spacing: 16) {
+                Text("電話番号を入力してください")
+                    .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                    .font(.system(size: 20, weight: .bold))
+                    .padding(.top, 68)
+
+                Text("BuyByFriends で快適に友達との売り買いを楽しむために電話番号入力をして本人確認をしましょう。")
+                    .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                    .font(.system(size: 14, weight: .medium))
+                    .padding(.bottom, 24)
+
+                VStack(spacing: 50) {
+                    HStack(spacing: 15) {
+                        Text("+81")
+                            .font(.system(size: 18, weight: .medium))
+                            .padding(.leading, 20)
+                            .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                        Text("|")
+                            .font(.system(size: 50, weight: .thin))
+                            .padding(.bottom, 5)
+                            .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                        TextField("000 0000 0000", text: vm.$binding.phoneNumber)
+                            .font(.system(size: 24, weight: .medium))
+                            .keyboardType(.phonePad)
+                            .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                    }
+                    .background(Asset.Colors.lightGray.swiftUIColor)
+                    .cornerRadius(12)
+
+                    Button(action: {
+                        UIApplication.shared.closeKeyboard()
+                        vm.input.startToLogIn.send()
+                    }) {
+                        Text("認証コードを送信")
+                            .foregroundColor(!vm.output.isEnabledLogInButton ? Asset.Colors.white.swiftUIColor:Asset.Colors.jetBlack.swiftUIColor)
+                            .frame(maxWidth: .infinity, minHeight: 60)
+                            .font(.system(size: 20, weight: .medium))
+                    }
+                    .accentColor(Color.white)
+                    .background(!vm.output.isEnabledLogInButton ? Asset.Colors.silver.swiftUIColor:Asset.Colors.chromeYellow.swiftUIColor)
+                    .cornerRadius(.infinity)
+                    .disabled(!vm.output.isEnabledLogInButton)
+                }
+                Spacer()
+            }
+            .frame(maxWidth: UIScreen.main.bounds.width*0.85)
+            .onChange(of: vm.output.isMovedVerificationCodeView) { _ in
+                path.path.append(Destination.Welcome.finishLogIn)
+            }
+            .onTapGesture {
+                UIApplication.shared.closeKeyboard()
+            }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        path.path.removeLast()
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(Asset.Colors.jetBlack.swiftUIColor)
+                    }
                 }
             }
         }
@@ -83,5 +86,6 @@ struct LogInView: View {
 struct LogInView_Previews: PreviewProvider {
     static var previews: some View {
         LogInView()
+            .environmentObject(Path())
     }
 }
